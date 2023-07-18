047abaffb9028201b8112ccae323fab896c54c9a
Manikandan-1811 <manisan2022@gmail.com>
2023-07-18 21:56:55 +0530

    config server code changes

--- src/test/java/com/jtech/cloud/config/CloudConfigServerApplicationTests.java
+++ src/test/java/com/jtech/cloud/config/CloudConfigServerApplicationTests.java
@@ -1,13 +1,19 @@
 package com.jtech.cloud.config;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class CloudConfigServerApplicationTests {
 
+	@Autowired
+	private CloudConfigServerApplication application;
 	@Test
 	void contextLoads() {
+		assertThat(application).isNotNull();
 	}
 
 }
